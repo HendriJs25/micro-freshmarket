190684ad6997c087bbe72040edd462e4ac992c9c
Hendri <hendri@ankh-systems.co.jp>
2026-08-18 17:59:48 +0900

    add function find by user id in role repository

--- user-service/repository/role/repository.go
+++ user-service/repository/role/repository.go
@@ -16,6 +16,7 @@ type repository struct {
 
 type Repository interface {
 	FindByName(context.Context, string) (*model.Role, error)
+	FindByUserID(context.Context, int64) ([]model.Role, error)
 }
 
 func NewRepository(db *gorm.DB) Repository {
@@ -34,4 +35,23 @@ func (r *repository) FindByName(ctx context.Context, name string) (*model.Role, 
 		return nil, fmt.Errorf("find role by name: %w", err)
 	}
 	return &role, nil
+}
+
+func (r *repository) FindByUserID(ctx context.Context, userID int64) ([]model.Role, error) {
+	var roles []model.Role
+
+	err := r.db.WithContext(ctx).Model(&model.Role{}).Joins(
+		`JOIN user_role 
+				ON user_role.role_id = roles.id
+				AND user_role.deleted_at IS NULL`).Where("user_role.user_id = ?", userID).Order("users.id ASC").Find(&roles).Error
+
+	if err != nil {
+		return nil, fmt.Errorf("find roles by user id: %w", err)
+	}
+
+	if len(roles) == 0 {
+		return nil, fmt.Errorf("find roles by user id: %w", apperror.ErrNotFound)
+	}
+
+	return roles, nil
 }
