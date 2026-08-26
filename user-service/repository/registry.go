901fbdb8027b7e19806966ff68877f58ffbe8846
Hendri <hendri@ankh-systems.co.jp>
2026-08-26 17:53:15 +0900

    add customer repository and service

--- user-service/repository/registry.go
+++ user-service/repository/registry.go
@@ -1,6 +1,7 @@
 package repository
 
 import (
+	"user-service/repository/customer"
 	"user-service/repository/role"
 	"user-service/repository/user"
 	"user-service/repository/userrole"
@@ -15,6 +16,7 @@ type Registry struct {
 	UserRole          userrole.Repository
 	VerificationToken verificationtoken.Repository
 	Transaction       TransactionManager
+	Customer          customer.Repository
 }
 
 func NewRegistry(db *gorm.DB) *Registry {
@@ -24,5 +26,6 @@ func NewRegistry(db *gorm.DB) *Registry {
 		UserRole:          userrole.NewRepository(db),
 		VerificationToken: verificationtoken.NewRepository(db),
 		Transaction:       NewTransactionManager(db),
+		Customer:          customer.NewRepository(db),
 	}
 }
--- user-service/services/registry.go
+++ user-service/services/registry.go
@@ -5,15 +5,17 @@ import (
 	"user-service/config"
 	"user-service/repository"
 	sessionrepository "user-service/repository/session"
+	"user-service/services/customer"
 	jwtservice "user-service/services/jwt"
 	roleservice "user-service/services/role"
 	userservice "user-service/services/user"
 )
 
 type Registry struct {
-	User userservice.Service
-	JWT  jwtservice.Service
-	Role roleservice.Service
+	User     userservice.Service
+	JWT      jwtservice.Service
+	Role     roleservice.Service
+	Customer customer.Service
 }
 
 func NewRegistry(repositories *repository.Registry, jwtConfig config.JWT, sessionRepository sessionrepository.Repository) (*Registry, error) {
@@ -29,7 +31,8 @@ func NewRegistry(repositories *repository.Registry, jwtConfig config.JWT, sessio
 			repositories.Transaction,
 			jwtService,
 			sessionRepository),
-		JWT:  jwtService,
-		Role: roleservice.NewService(repositories.Role),
+		JWT:      jwtService,
+		Role:     roleservice.NewService(repositories.Role),
+		Customer: customer.NewService(repositories.Customer),
 	}, nil
 }
